0ff3071b808cf500c932e4c3e053d50be5bd80b1
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-11-22 13:37:49 -0800

    Added jason string

--- myapp/Program.cs
+++ myapp/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
-
+using Newtonsoft.Json;
 namespace myapp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            var product=new Product{ID=100,Name="Mandeep Kaur",Price=99.9};
+        //Serialize the product object to json
+        var jsonstring=JsonConvert.SerializeObject(product);
+        Console.WriteLine(jsonstring);
+
         }
+    }
+    class Product{
+        public int ID{get;set;}
+        public string Name{get;set;}
+        public double Price{get;set;}
     }
 }
